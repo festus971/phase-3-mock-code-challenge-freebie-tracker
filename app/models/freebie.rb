b1952fa8b7a5934a7ed958fe6061610cfbc56594
+ <+>
2022-08-29 11:51:20 +0300

    finish

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,9 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :Company
+    belongs_to :dev
+
+    def print_details
+        "#{self.dev.name} owns a #{self.item_name} from #{self.Company.name}"
+    end
 
 end
